ed8b89352389738cd6a2bdc0b106851944cfc3d6
bibiana <bcristofol@gmail.com>
2016-12-05 10:36:42 +0000

    Render html form with Tornado

---
+++ async_web_app.py
@@ -0,0 +1,28 @@
+# CHECK ORDER
+import os
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+        ]
+        settings = {
+            "debug": True,
+            "static_path": os.path.join(os.path.dirname(__file__), "bootstrap")
+        }
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+if __name__ == "__main__":
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
